63913e25e700c2b132c698b23ffd84b088901d80
João Antonio Pereira de Sousa e Silva <catce.2025111mtds0065@aluno.ifpi.edu.br>
2026-04-27 20:27:37 -0300

    Delete Caffeoteca_projeto_integrador_2026-TDS-IFPI directory

--- Caffeoteca_projeto_integrador_2026-TDS-IFPI/repositorio/backend/routes/carrinho_router.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from controllers import carrinho_controller
-
-def adicionar_rotas_carrinho(app: Flask):
-    app.add_url_rule('/carrinho', view_func=carrinho_controller.carrinho, methods=['GET', 'POST'])
-    app.add_url_rule('/carrinho/remover/<int:item_id>', view_func=carrinho_controller.remover_item, methods=['POST'])
-    app.add_url_rule('/carrinho/aumentar/<int:item_id>', view_func=carrinho_controller.aumentar_quantidade, methods=['POST'])
-    app.add_url_rule('/carrinho/diminuir/<int:item_id>', view_func=carrinho_controller.diminuir_quantidade, methods=['POST'])
--- Caffeoteca_projeto_integrador_2026-TDS-IFPI/repositorio/database/schema.sql
+++
@@ -1,8 +0,0 @@
--- Schema for Caffeoteca database
-
-CREATE TABLE IF NOT EXISTS produtos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
-    descricao TEXT,
-    preco REAL NOT NULL
-);
